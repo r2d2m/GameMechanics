fa8adfbbe2572faec60b51c81ef1a685584da0c2
DESKTOP-9LA1EMR\Vishal <vishalb98@gmail.com>
2017-04-22 15:48:00 +0530

    Sailboat update

---
+++ Assets/_Vechicles/Sailboat/Scripts/SailboatController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SailboatController : MonoBehaviour {
+
+    public float m_TurnInput;
+    public bool isSailing;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetAxis("Vertical") > 0)
+            isSailing = true;
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow) || Input.GetAxis("Vertical") < 0)
+            isSailing = false;
+
+        m_TurnInput = Input.GetAxis("Horizontal");
+    }
+}
---
+++ Assets/_Vechicles/Sailboat/Scripts/SailboatMotor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SailboatMotor : MonoBehaviour {
+
+    private Rigidbody rb_sailboat;
+
+    public float m_Speed = 5f;
+    public float m_TurnSpeed = 2f;
+    public float m_SailboatVelocity;
+    public float m_MaxAngularVelocity = 30;
+    public float m_MaxForwardVelocity = 25;
+
+    public float m_UpwardBouyantForce = 19.6f;
+
+    public SailboatController sail_Input;
+
+    public Vector3 windForce = new Vector3(5f,0,0);
+    public Vector3 sailBoatDirection;
+
+    public Transform[] rays = new Transform[2];
+
+    private void Start()
+    {
+        rb_sailboat = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        Bouyancy();
+
+        float windFacing = Vector3.Dot(windForce.normalized, gameObject.transform.forward);
+        float angle = Mathf.Acos(-windFacing);// * Mathf.Rad2Deg;
+        
+        rb_sailboat.AddRelativeTorque(0, sail_Input.m_TurnInput * m_TurnSpeed, 0, ForceMode.Acceleration);
+
+        rb_sailboat.angularVelocity = Vector3.ClampMagnitude(rb_sailboat.angularVelocity, m_MaxAngularVelocity);
+
+        m_SailboatVelocity = Vector3.Dot(windForce, gameObject.transform.right);
+
+        rb_sailboat.AddRelativeForce(gameObject.transform.forward * m_SailboatVelocity * m_Speed, ForceMode.Force);
+
+        rb_sailboat.velocity = Vector3.ClampMagnitude(rb_sailboat.velocity, m_MaxForwardVelocity);
+
+    }
+
+    private void Bouyancy()
+    {
+        if (Physics.Raycast(rays[0].position, Vector3.down, .5f))
+        {
+            Debug.Log("Hitting Water Left");
+            rb_sailboat.AddForceAtPosition(Vector3.up * m_UpwardBouyantForce, rays[0].position);
+        }
+        if (Physics.Raycast(rays[1].position, Vector3.down, .5f))
+        {
+            Debug.Log("Hitting Water Right");
+            rb_sailboat.AddForceAtPosition(Vector3.up * m_UpwardBouyantForce, rays[0].position);
+        }
+    }
+}
