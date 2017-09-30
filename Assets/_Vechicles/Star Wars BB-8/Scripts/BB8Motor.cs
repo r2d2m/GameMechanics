5dc940ef82da7b6cfa7af58c9b751120ce9c5c46
Vishal Bilagi <vishalb98@gmail.com>
2017-09-30 17:40:45 +0530

    Fixed body rotation to have a realistic effect

--- Assets/_Vechicles/Star Wars BB-8/Scripts/BB8Motor.cs
+++ Assets/_Vechicles/Star Wars BB-8/Scripts/BB8Motor.cs
@@ -7,20 +7,43 @@ public class BB8Motor : MonoBehaviour {
     private BB8Controller BB8Controller;
     private Rigidbody BB8Rigidbody;
     private Transform Body;
-    private Transform Head;
+
+    private float oldXaxisDistance;
+    private float currentXaxisDistance;
+    private float travelledXaxisDistance;
+    private float xAxisAngle;
+
+    private float oldZaxisDistance;
+    private float currentZaxisDistance;
+    private float travelledZaxisDistance;
+    private float zAxisAngle;
 
     public float movementSpeed;
+
     void Start()
     { 
         BB8Controller = GetComponent<BB8Controller>();
         Body = transform.GetChild(0).GetComponent<Transform>();
-        Head = transform.GetChild(1).GetComponent<Transform>();
     }
 
     void Update () {
+
+
+        oldXaxisDistance = gameObject.transform.position.x;
+        oldZaxisDistance = gameObject.transform.position.z;
+                
         gameObject.transform.position += transform.TransformDirection(BB8Controller.movementInput) * Time.deltaTime * movementSpeed;
 
-        Body.Rotate(new Vector3(BB8Controller.movementInput.z, 0 , -BB8Controller.movementInput.x), movementSpeed , Space.World);
+        currentXaxisDistance = gameObject.transform.position.x;
+        currentZaxisDistance = gameObject.transform.position.z;
+
+        travelledXaxisDistance = currentXaxisDistance - oldXaxisDistance;
+        travelledZaxisDistance = currentZaxisDistance - oldZaxisDistance;
+
+        xAxisAngle = (travelledXaxisDistance * 360) / (2 * Mathf.PI * .656f);
+        zAxisAngle = (travelledZaxisDistance * 360) / (2 * Mathf.PI * .656f);
+        
+        Body.Rotate(zAxisAngle, 0, -xAxisAngle, Space.World);
         
 	}
 }
